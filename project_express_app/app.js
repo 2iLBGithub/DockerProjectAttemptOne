16e8b10e57b3aa339995b694bb0ec9d5bc2387d3
Lewis Brennan <lewis.brennan@2itesting.com>
2023-11-17 10:48:26 +0000

    moved fetching from react app to express

--- project_express_app/app.js
+++ project_express_app/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fetch = require('node-fetch'); // Ensure you have node-fetch installed
 const app = express();
 const port = 3001; 
 app.use(cors({ origin: 'http://localhost:3000' }));
@@ -9,9 +10,22 @@ app.get('/', (req, res) => {
 });
 
 app.get('/message', (req, res) => {
-  res.json({message: 'Bonjour from Express!'});
+  res.json({message: 'Hello there from Express!'});
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+app.get('/fetch-deck', async (req, res) => {
+  const response = fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+  const data = await response.json();
+  res.json(data);
+})
+
+app.get('/draw-deck/:deckId', async (req, res) => {
+  const deckId = req.params.deckId;
+  const response = await fetch('https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1')
+  const data = await response.json();
+  res.json(data);
+})
